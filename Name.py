464ea8900bdaf523b521887f730956b7378008b9
Syed VScode <syedvscode.com>
2025-07-16 09:36:47 +0530

    Initial commit: setup basic web app files

---
+++ Name.py
@@ -0,0 +1,6 @@
+# Ask for user's name and age
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Output the entered name and age
+print(f"Your name is {name} and your age is {age}.")
